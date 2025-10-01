d041a07db3568c07d402ff54ea277c901e341150
Athov07 <atharvadsare17@gmail.com>
2025-10-01 05:53:00 +0530

    Cloudinary and Multer Configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        //Upload file on cloudinary
+
+        const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"})
+
+    //File uploaded on cloudinary
+    console.log("File uploaded on cloudinary", response.url);
+    return response;
+    } 
+    catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.log("Error while uploading file on cloudinary", error);
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary};
