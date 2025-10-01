fd69158d35f237f5fd56dc47e7a2bbafdc4ae6cd
Athov07 <atharvadsare17@gmail.com>
2025-10-01 09:22:42 +0530

    All Setup Configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,19 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+
+// Import routes and use them
+import userRouter from "./routes/user.routes.js";
+
+
+
+// Declaration of routes
+app.use("/api/v1/users", userRouter);
+
+
+
+
+
+
 export {app};
--- src/index.js
+++ src/index.js
@@ -2,22 +2,15 @@
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants.js";
 import connectDB from "./db/index.js";
-
-
+import { app } from "./app.js";
 import dotenv from "dotenv";
 dotenv.config({path: "./.env"});
 
 connectDB()
 .then(() => {
-    import("./app.js").then(({default: app}) => {
-        app.on("error", (error) => {
-            console.error("Error starting the server:", error);
-            throw error;
-        });
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server started on port ${process.env.PORT}`);
-        });
-    });
+        })
     })
 .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
